d0935dc1cb9e35898cc97ce21afacf016c68eaa3
Gabriel Bueno <106624338+gabriel-viniciusbueno@users.noreply.github.com>
2023-03-31 09:49:08 -0300

    HTML e CSS do projeto

---
+++ ProgramadorBr/js-progbr/projetos/d01-churrascometro/script.js
@@ -0,0 +1,42 @@
+let inputAdultos = document.getElementById('adultos')
+let inputCriancas = document.getElementById('criancas')
+let inputDuracao = document.getElementById('duracao')
+let calcBtn = document.getElementById('calcular')
+
+let resultado = document.getElementById('resultado')
+
+calcBtn.addEventListener('click', function(){
+    const adultos = inputAdultos.value
+    const criancas = inputCriancas.value
+
+    let totalCarne = adultos * carnePP(duracao) + (criancas * carnePP(duracao) / 2)
+    
+    let totalCerveja = adultos * cervejaPP(duracao)
+    let totalBebidas = adultos * bebidasPP(duracao) + (criancas * bebidasPP(duracao) / 2)
+})
+
+function carnePP (duracao){
+    if(duracao >= 6){
+        return 650
+    } else{
+        return 400
+    }
+}
+
+function cervejaPP (duracao){
+    if(duracao >= 6){
+        return 2000
+    } else{
+        return 1200
+    }
+}
+
+function bebidasPP (duracao){
+    if(duracao >= 6){
+        return 1500
+    } else{
+        return 1000
+    }
+}
+
+
