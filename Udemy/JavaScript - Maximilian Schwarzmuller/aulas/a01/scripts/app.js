a0283bc6844cf08049a0c05c2e1dac4e35c33164
Gabriel Bueno <106624338+gabriel-viniciusbueno@users.noreply.github.com>
2023-04-10 20:43:15 -0300

    Outros operadores

--- Udemy/JavaScript - Maximilian Schwarzmuller/aulas/a01/scripts/app.js
+++ Udemy/JavaScript - Maximilian Schwarzmuller/aulas/a01/scripts/app.js
@@ -1,12 +1,42 @@
 let defaultResult = 0
 let currentResult = defaultResult
 
-function add(){
-    const enteredNumber = parseFloat(userInput.value)
-    const calcDescription =  `${currentResult} + ${enteredNumber}`
-    currentResult += enteredNumber
+
+function createOutput(operator, resultBeforeCalc, calcNumber) {
+    const calcDescription =  `${resultBeforeCalc} ${operator} ${calcNumber}`
     outputResult(currentResult, calcDescription)
 }
 
+function add(){
+    const enteredNumber = parseFloat(userInput.value)
+    const initialResult = currentResult;
+    currentResult += enteredNumber
+    createOutput('+', initialResult, enteredNumber)
+}
+
+function subtract() {
+    const enteredNumber = parseFloat(userInput.value)
+    const initialResult = currentResult;
+    currentResult -= enteredNumber
+    createOutput('-', initialResult, enteredNumber)
+}
+
+function multiply() {
+    const enteredNumber = parseFloat(userInput.value)
+    const initialResult = currentResult;
+    currentResult *= enteredNumber
+    createOutput('*', initialResult, enteredNumber)
+}
+
+function divide() {
+    const enteredNumber = parseFloat(userInput.value)
+    const initialResult = currentResult;
+    currentResult /= enteredNumber
+    createOutput('/', initialResult, enteredNumber)
+}
+
 addBtn.addEventListener('click', add)
+subtractBtn.addEventListener('click', subtract)
+multiplyBtn.addEventListener('click', multiply)
+divideBtn.addEventListener('click', divide)
 
