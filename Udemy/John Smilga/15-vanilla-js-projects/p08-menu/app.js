5eb4834feb34ff6769e2d548ece9379a0fd998e6
Gabriel Bueno <106624338+gabriel-viniciusbueno@users.noreply.github.com>
2023-05-04 14:56:14 -0300

    Finalizando o projeto

--- Udemy/John Smilga/15-vanilla-js-projects/p08-menu/app.js
+++ Udemy/John Smilga/15-vanilla-js-projects/p08-menu/app.js
@@ -71,13 +71,57 @@ const menu = [
     img: "./images/item-9.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
+  {
+    id: 10,
+    title: "steak dinner",
+    category: "dinner",
+    price: 39.99,
+    img: "./images/item-10.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
 ]
 
 const sectionCenter = document.querySelector('.section-center')
 
+const container = document.querySelector('.btn-container')
+
+// load items
 window.addEventListener('DOMContentLoaded', function () {
- displayMenuItems(menu)
+  displayMenuItems(menu)
+  
+  const categories = menu.reduce(function(values, item){
+    if(!values.includes(item.category)){
+      values.push(item.category)
+    }
+    return values
+  }, ['all'])
+  const categoryBtn = categories.map(function(category){
+    return `<button class="filter-btn" type="button" data-id="${category}">${category}</button>`
+    
+  }).join('')
+  container.innerHTML = categoryBtn
+  const filterBtn = document.querySelectorAll('.filter-btn')
+  // filter items
+  filterBtn.forEach(function(button){
+    button.addEventListener('click', function (e) {
+      const category = e.currentTarget.dataset.id;
+      const menuCategory = menu.filter(function(menuItem){
+        if(menuItem.category === category){
+          return menuItem
+        }
+      })
+      if(category === 'all'){
+        displayMenuItems(menu)
+      } else{
+        displayMenuItems(menuCategory)
+      }
+    })
+  })
 })
+
+
+
+
 
 function displayMenuItems(menuItems){
   let displayMenu = menuItems.map(function(item){
