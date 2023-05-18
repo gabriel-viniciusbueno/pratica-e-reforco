eba2b037c2476c3fe3e5dbb70efd4800684af6fb
Gabriel Bueno <106624338+gabriel-viniciusbueno@users.noreply.github.com>
2023-05-18 18:42:32 -0300

    Embaralhando as cartas

--- ProgramadorBr/remake-html-css-progbr/desafios/d06-jogo-da-memoria/scripts/script.js
+++ ProgramadorBr/remake-html-css-progbr/desafios/d06-jogo-da-memoria/scripts/script.js
@@ -12,6 +12,25 @@ let techs = ['bootstrap',
         'node',
         'react']
 
+let cards = null
+startGame()
+function startGame(){
+    cards = createCardsFromTechs(techs)
+    shuffleCards(cards)
+    console.log(cards)
+}
+
+function shuffleCards(cards){
+    let currentIndex = cards.length
+    let randomIndex = 0
+
+    while(currentIndex !== 0){
+        randomIndex = Math.floor(Math.random() * currentIndex)
+        currentIndex--
+        [cards[randomIndex], cards[currentIndex]] = [cards[currentIndex], cards[randomIndex]]
+    }
+}
+
 createCardsFromTechs(techs)
 function createCardsFromTechs(techs){
     let cards = []
