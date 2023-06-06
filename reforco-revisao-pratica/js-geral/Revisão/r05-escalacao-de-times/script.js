f4e9514e6aa72f500d65012f9aed71f28b14272b
Gabriel Bueno <106624338+gabriel-viniciusbueno@users.noreply.github.com>
2023-06-06 17:20:28 -0300

    Funcionalidade deletar

--- reforco-revisao-pratica/js-geral/Revisão/r05-escalacao-de-times/script.js
+++ reforco-revisao-pratica/js-geral/Revisão/r05-escalacao-de-times/script.js
@@ -2,24 +2,38 @@ const playerName = document.getElementById('name')
 const playerPosition = document.getElementById('position')
 const playerNumber = document.getElementById('number')
 const addBtn = document.getElementById('add-player-btn')
-const sectionTeam = document.getElementById('time')
+const ulTeam = document.getElementById('team')
+const removePlayer = document.getElementById('remove-player-btn')
 
 addBtn.addEventListener('click', (e) => {
   e.preventDefault()
 
 
-  const createList = document.createElement('ul')
   const playerLi = document.createElement('li')
 
   const confirmacao = confirm(playerName.value + ' é ' + playerPosition.value + ' e camisa ' + playerNumber.value + '?')
   if(confirmacao){
-    playerLi.id = 'player' + playerNumber
+    playerLi.id = 'player' + playerNumber.value
+    playerLi.className = 'players'
     playerLi.innerHTML = 'player: ' + playerName.value + ' (' + playerNumber.value + ') ' + playerPosition.value
-    createList.appendChild(playerLi)
-    sectionTeam.appendChild(createList)
+
+    ulTeam.appendChild(playerLi)
   }
+})
+
+
+removePlayer.addEventListener('click', (e) => {
+  e.preventDefault()
+  const players = document.querySelectorAll('.players')
+  const numberInput = document.getElementById('number-to-delete').value
+  console.log(numberInput)
+
+  players.forEach(function(player){
+    if(player.id == 'player' + numberInput){
+      ulTeam.removeChild(player)
+    }
+  })
 
   
   
 })
-
