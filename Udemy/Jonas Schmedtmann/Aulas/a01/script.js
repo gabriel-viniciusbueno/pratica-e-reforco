830969103ab40d84ff2d158b1e0c4b4a48e7d3c9
Gabriel Bueno <106624338+gabriel-viniciusbueno@users.noreply.github.com>
2023-06-26 15:21:07 -0300

    Includes e IndexOf

---
+++ Udemy/Jonas Schmedtmann/Aulas/a01/script.js
@@ -0,0 +1,8 @@
+const friends = ['Bob', 'Jake', 'Chris']
+
+console.log(friends.indexOf('Bob'))
+console.log(friends.indexOf('Jessica'))
+// IndexOf retorna -1 quando não encontra o elemento no array
+
+console.log(friends.includes('Bob'))
+// Includes também pode ser usado para descobrir se o elemento se encotra no array. Retorna um boolean
